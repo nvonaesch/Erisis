29a442f2077d6d4e28262d2521a16150f948038c
nvonaesch <nicolas.vonaesch1@gmail.com>
2025-01-13 09:24:30 +0100

    AGAGOUGOU

--- Assets/Scripts/Scene1/DupliquerObjetPosition.cs
+++ Assets/Scripts/Scene1/DupliquerObjetPosition.cs
@@ -5,22 +5,32 @@ using UnityEngine;
 
 public class DupliquerObjetPosition : MonoBehaviour
 {
+   
     public GameObject objADupliquer;
-    private GameObject duplicata;
-    private Vector3 localPosition;
     void Start()
     {
         StartCoroutine(Dupliquer());
-        duplicata = objADupliquer;
     }
 
     IEnumerator Dupliquer()
     {
-        yield return new WaitForSeconds(5);
-        localPosition = objADupliquer.transform.localPosition;
-        localPosition.y+=1; 
-        duplicata.transform.SetLocalPositionAndRotation(localPosition, objADupliquer.transform.localRotation);
-        Destroy(duplicata.GetComponent("DupliquerObjetPosition"));
-        Instantiate (duplicata, duplicata.transform);   
+        yield return new WaitForSeconds(2);
+        for (int i = 0; i < 30; i++)
+        {
+            yield return new WaitForSeconds((float)0.1);
+
+            Vector3[] positions = {
+            new Vector3((float)-0.578516,(float)5.486312,(float)-1.106001),
+            new Vector3((float)2.85,(float)5.486312,(float)-1.106001),
+            new Vector3((float)8.77,(float)5.486312,(float)-1.106001),
+            new Vector3((float)-3.58,(float)5.486312,(float)-1.106001),
+            new Vector3((float)-8.51,(float)5.486312,(float)-1.106001),
+        };
+
+            Vector3 position = positions[Random.Range(0, positions.Length)];
+            GameObject duplicata = Instantiate(objADupliquer, position, objADupliquer.transform.rotation);
+
+            Destroy(duplicata.GetComponent<DupliquerObjetPosition>());
+        }
     }
 }
