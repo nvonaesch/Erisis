47623c41aeb3d86eda1d63f6dc1573e750ac16c9
Nicolas VON_AESCH <e2302916@ensimlan.univ-lemans.fr>
2025-02-03 10:53:08 +0100

    Projet 2 gaia

---
+++ Assets/Polytope Studio/Lowpoly_Demos/Environment_Free/Helpers/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 5f;
+    public float gravity = -9.18f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    void Update()
+    {
+        // Vérifier si le joueur est au sol
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        // Augmenter la vitesse si "Left Shift" est pressé
+        if (Input.GetKey("left shift") && isGrounded)
+        {
+            speed = 10f;
+        }
+        else
+        {
+            speed = 5f;
+        }
+
+        // Déplacement horizontal
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        // Sauter
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // Appliquer la gravité
+        velocity.y += gravity * Time.deltaTime;
+
+        // Déplacement vertical
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
