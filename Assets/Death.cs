f37236b7ad61a8729af779ecdd7252589487f0c8
Nicolas VON_AESCH <e2302916@ensimlan.univ-lemans.fr>
2025-02-07 13:44:46 +0100

    La plus belle scene du monde

--- Assets/Death.cs
+++ Assets/Death.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class Death : MonoBehaviour
 {
-    public String Scene1
+    public Scene Scene1;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +21,6 @@ public class Death : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene(Scene1);
+        SceneManager.LoadScene("Scene1");
     }
 }
