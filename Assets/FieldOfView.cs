75ddaafa61b9b2218f48309f91f9115496730761
Nicolas VON_AESCH <e2302916@ensimlan.univ-lemans.fr>
2025-02-11 18:20:52 +0100

    manque de buzz

---
+++ Assets/FieldOfView.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float angle;
+    public float radius;
+    public GameObject playerRef;
+    public LayerMask targetMask;
+    public LayerMask obstructionMask;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
