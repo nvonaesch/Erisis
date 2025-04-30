7961b86a42add9f31f0ac18792e2daec0d0051b9
Nicolas VON_AESCH <e2302916@ensimlan.univ-lemans.fr>
2025-04-30 12:17:17 +0200

    Marche fluide de Thanatos mais que dans un sens

--- Assets/animation.cs
+++ Assets/animation.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class animation : MonoBehaviour
 {
     private Animator animator;
+    private string moveStateName = "Walk 0";
     private bool alreadyTriggered = false;
 
     // Start is called before the first frame update
@@ -17,6 +18,10 @@ public class animation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        if (stateInfo.IsName(moveStateName))
+        {
+            transform.position += transform.forward * Time.deltaTime;
+        }
     }
 }
