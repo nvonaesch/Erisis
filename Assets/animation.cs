7c7315fc5d072e868666f471d7a8b5fe28991f2e
Nicolas VON_AESCH <e2302916@ensimlan.univ-lemans.fr>
2025-04-30 12:30:17 +0200

    Ajout du FOV sur thanatos mais bug avec son centre

--- Assets/animation.cs
+++ Assets/animation.cs
@@ -7,11 +7,161 @@ public class animation : MonoBehaviour
     private Animator animator;
     private string moveStateName = "Walk 0";
     private bool alreadyTriggered = false;
+    public GameObject buzz;
+    public GameObject center;
+    public float radius;
+    [Range(0, 360)]
+    public float angle;
+
+    public float Speed = 2f;
+    public int segments = 20;
+
+    public GameObject playerRef;
+
+    [Range(0, 100)]
+    public float alert;
+
+    public bool canSeePlayer;
+    public bool hasRotated = false;
+
+    public LayerMask targetMask;
+    public LayerMask obstructionMask;
+
+    private MeshFilter meshFilter;
+    private MeshRenderer meshRenderer;
+    private Mesh mesh;
+    public GameObject targetObject1;
+    public GameObject targetObject2;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        StartCoroutine(FOVRoutine());
+
+        meshFilter = gameObject.AddComponent<MeshFilter>();
+        meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        meshRenderer.material = new Material(Shader.Find("Standard"));
+        mesh = new Mesh();
+        meshFilter.mesh = mesh;
+        DrawFieldOfView();
+
+    }
+    private IEnumerator FOVRoutine()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.2f);
+        while (true)
+        {
+            yield return wait;
+            FieldOfViewCheck();
+        }
+    }
+
+    private void FieldOfViewCheck()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+        MaterialPropertyBlock mpb = new MaterialPropertyBlock();
+        meshRenderer.GetPropertyBlock(mpb);
+        if (rangeChecks.Length != 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
+                {
+                    canSeePlayer = true;
+                }
+                else
+                {
+                    canSeePlayer = false;
+                }
+            }
+            else
+            {
+                canSeePlayer = false;
+            }
+        }
+        else if (canSeePlayer)
+        {
+            canSeePlayer = false;
+        }
+        if (canSeePlayer && alert < 100)
+        {
+            alert += 5;
+        }
+        if (!canSeePlayer && alert > 0)
+        {
+            alert--;
+        }
+
+        if (alert >= 100)
+        {
+            mpb.SetColor("_Color", Color.red);
+            meshRenderer.SetPropertyBlock(mpb);
+        }
+        if (50 <= alert && alert < 100)
+        {
+            Color orange = new Color(1.0f, 0.5f, 0.0f);
+            mpb.SetColor("_Color", orange);
+            meshRenderer.SetPropertyBlock(mpb);
+        }
+        else if (alert < 50)
+        {
+            mpb.SetColor("_Color", Color.yellow);
+            meshRenderer.SetPropertyBlock(mpb);
+        }
+
+    }
+
+    private Vector3 DirectionFromAngle(float eulerY, float angleInDegrees)
+    {
+        angleInDegrees += eulerY;
+
+
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
+    void DrawFieldOfView()
+    {
+        Vector3 frontLeft = DirectionFromAngle(transform.eulerAngles.y, -angle / 2) * radius;
+        Vector3 frontRight = DirectionFromAngle(transform.eulerAngles.y, angle / 2) * radius;
+
+
+        int stepCounts = segments + 1;
+        float stepAngleSize = angle / (float)segments;
+        List<Vector3> vertices = new List<Vector3> { Vector3.up * 2.5f }; // Point central du cône
+        List<int> triangles = new List<int>();
+        for (int i = 0; i <= stepCounts; i++)
+        {
+            float currentAngle = -angle / 2 + stepAngleSize * i;
+            Vector3 vertex = DirectionFromAngle(transform.eulerAngles.y, currentAngle) * radius + (Vector3.up * 0.7f);
+            vertices.Add(vertex.normalized * radius * (1 / transform.localScale.x));
+        }
+        for (int i = 1; i < vertices.Count - 1; i++)
+        {
+            triangles.Add(0);
+            triangles.Add(i);
+            triangles.Add(i + 1);
+        }
+        mesh.Clear();
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateNormals();
+        MaterialPropertyBlock mpb = new MaterialPropertyBlock();
+        meshRenderer.GetPropertyBlock(mpb);
+        if (canSeePlayer)
+        {
+            mpb.SetColor("_Color", Color.red);
+        }
+        else
+        {
+            mpb.SetColor("_Color", Color.yellow);
+        }
+        meshRenderer.SetPropertyBlock(mpb);
 
     }
 
